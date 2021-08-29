985db6a24242e82e2ceb093d0c73d4d3f4281af4
Nathan McAvoy <nathanmc158@gmail.com>
2021-08-29 15:47:42 +0100

    Make home responsive and add unauth version

--- sns-frontend/src/Home.jsx
+++ sns-frontend/src/Home.jsx
@@ -6,8 +6,7 @@ import { useGetSignInStatusQuery } from './services/auth';
 
 const useStyles = makeStyles({
   card: {
-    width: 275,
-    marginRight: 20
+    margin: '1rem',
   },
 });
 
@@ -15,45 +14,46 @@ const Home = () => {
   const signInStatus = useGetSignInStatusQuery();
   const classes = useStyles();
 
-  if (signInStatus.data === "none")
-    return (
-      <Redirect to="/login" />
-    );
+  
   if (signInStatus.data === "student")
     return (
       <>
         <h1>Home</h1>
-        <Grid container justifyContent="flex-start" spacing={2} wrap="wrap">
-          <Card className={classes.card}>
-            <CardContent>
-              <Typography variant="h5" component="h2">
-                Create a Report
-              </Typography>
-              <Typography color="textSecondary">
-                Need to let someone know about something? Want to talk?
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button component={RouterLink} size="small" to="/create">
-                Create
-              </Button>
-            </CardActions>
-          </Card>
-          <Card className={classes.card}>
-            <CardContent>
-              <Typography variant="h5" component="h2">
-                Check Report Status
-              </Typography>
-              <Typography color="textSecondary">
-                What's happened with that report?
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button component={RouterLink} size="small" to="/my_reports">
-                Check
-              </Button>
-            </CardActions>
-          </Card>
+        <Grid container>
+          <Grid item xs={12} sm={6}>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Create a Report
+                </Typography>
+                <Typography color="textSecondary">
+                  Need to let someone know about something? Want to talk?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button component={RouterLink} size="small" to="/create">
+                  Create
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Check Report Status
+                </Typography>
+                <Typography color="textSecondary">
+                  What's happened with that report?
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button component={RouterLink} size="small" to="/my_reports">
+                  Check
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
       </>
     );
@@ -61,43 +61,85 @@ const Home = () => {
     return (
       <>
         <h1>Home</h1>
-        <Grid container justifyContent="center" spacing={2}>
-        <Grid container justifyContent="flex-start" spacing={2} wrap="wrap">
-          <Card className={classes.card}>
-            <CardContent>
-              <Typography variant="h5" component="h2">
-                Respond
-              </Typography>
-              <Typography color="textSecondary">
-                Respond to reports sent to you by your students.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button component={RouterLink} size="small" to="/reports">
-                Respond
-              </Button>
-            </CardActions>
-          </Card>
-          <Card className={classes.card}>
-            <CardContent>
-              <Typography variant="h5" component="h2">
-                Settings
-              </Typography>
-              <Typography color="textSecondary">
-                Configure your school's settings, questions and users.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button component={RouterLink} size="small" to="/school_settings">
-                Configure
-              </Button>
-            </CardActions>
-          </Card>
-        </Grid>
+        <Grid container spacing={1}>
+          <Grid item xs={12} sm={6}>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Respond
+                </Typography>
+                <Typography color="textSecondary">
+                  Respond to reports sent to you by your students.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button component={RouterLink} size="small" to="/reports">
+                  Respond
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  Settings
+                </Typography>
+                <Typography color="textSecondary">
+                  Configure your school's settings, questions and users.
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button component={RouterLink} size="small" to="/school_settings">
+                  Configure
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
         </Grid>
       </>
     );
-  return <></>
+  return (
+    <>
+      <h1>Home</h1>
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={6}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Login
+              </Typography>
+              <Typography color="textSecondary">
+                Login to your account.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button component={RouterLink} size="small" to="/login">
+                login
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Card className={classes.card}>
+            <CardContent>
+              <Typography variant="h5" component="h2">
+                Register
+              </Typography>
+              <Typography color="textSecondary">
+                Create an account.
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button component={RouterLink} size="small" to="/register">
+                Register
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default Home;
